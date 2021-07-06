9a1129583f31245f542d3bb9a7e2d30d72c8ee54
Mac-NOZ <mac-noz@Mac-mini-de-Mac-NOZ.local>
2021-07-06 17:30:02 -0300

    Sistema para medir velocidade do veiculo em js e html

---
+++ e009.js
@@ -0,0 +1,15 @@
+/* var vel = 60;
+console.log(`a velocidade do seu carro é ${vel} Km/h`);
+if(vel > 60){ // condiçao simples pois nao tem o else
+    console.log("voce ultrapassou o limite de velocidade")
+}
+console.log("dirija sempre usando cinto de segurança");
+*/
+
+var pais = "Brasil";
+
+if (pais == "Brasil"){
+    console.log("Brasileiro")
+}else {
+    console.log("Estrangeiro")
+}
